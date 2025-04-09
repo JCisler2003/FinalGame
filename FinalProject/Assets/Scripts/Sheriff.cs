d27b70e51f257f94e29cb4767ca60cac0e42246b
JCisler2003 <jcisler@siue.edu>
2025-04-09 18:40:56 -0500

    Fixed movement

--- FinalProject/Assets/Scripts/Sheriff.cs
+++ FinalProject/Assets/Scripts/Sheriff.cs
@@ -8,19 +8,35 @@ public class Sheriffs : Enemy
     [SerializeField] private Vector3 p0;
     [SerializeField] private float frequency = 1f; // speed of oscillation
     [SerializeField] private float amplitude = 2f; // range of up/down movement
+    [SerializeField] private float verticalSpeed = 2f;
+private int direction = 1;
     private float timeStart;
 
     void Start()
     {
         p0 = Vector3.zero;
         p0.x = bndCheck.camWidth + bndCheck.radius;
-        p0.y = Random.Range(-bndCheck.camHeight, bndCheck.camHeight);
+        p0.y = Random.Range(-bndCheck.camHeight, bndCheck.TopScreen);
     }
     public override void Move()
     {
-        Vector3 pos = p0;
-        float t = Time.time - timeStart;
-        pos.y += Mathf.Sin(t * frequency) * amplitude; // up/down oscillation
-        transform.position = pos;
+
+        // Vector3 pos = p0;
+        // float t = Time.time - timeStart;
+        // pos.y += Mathf.Sin(t * frequency) * amplitude; // up/down oscillation
+        // transform.position = pos;
+
+        Vector3 pos = transform.position;
+    pos.y += verticalSpeed * direction * Time.deltaTime;
+    transform.position = pos;
+
+    if (bndCheck.LocIs(BoundsCheck.eScreenLocs.offUp))
+    {
+        direction = -1;
+    }
+    else if (bndCheck.LocIs(BoundsCheck.eScreenLocs.offDown))
+    {
+        direction = 1;
+    }
     }
 }
