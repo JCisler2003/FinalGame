e9fef37e50fc1e63f552c4280c2202dea38dc772
nuresabri <nuresabri3@gmail.com>
2025-04-11 12:11:39 -0500

    updated

--- FinalProject/Assets/Scripts/Cowboy.cs
+++ FinalProject/Assets/Scripts/Cowboy.cs
@@ -3,36 +3,53 @@ using UnityEngine;
 
 public class Cowboy : MonoBehaviour
 {
-    static public Cowboy S{get; private set; }
+    static public Cowboy S { get; private set; }
 
     [Header("Inscribed")]
     public float speed = 10f;
-    public float jumpForce = 5f;
+    public float jumpForce = 10f;
     public float pitchMult = 30;
-    public Animator anim;
 
     [Header("Dynamic")] [Range(0, 4)]
-
     private float _shieldLevel = 1; // remember the underscore
-    [Tooltip( "This field holds a reference to the last triggering GameObject" )]
+    [Tooltip("This field holds a reference to the last triggering GameObject")]
     private GameObject lastTriggerGo = null;
-    private Rigidbody rb;
+    private Rigidbody2D rb;
     private bool isGrounded = true;
+
+    // Sprite flip support
+    private Transform visual;
+    private Vector3 originalScale;
+
+    // Reference to SpriteAnimator script
+    private SpriteAnimator spriteAnimator;
 
     //public delegate void WeaponFireDelegate();
     //public event WeaponFireDelegate fireEvent;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
-        if (S == null){
+        if (S == null)
+        {
             S = this;
-        }else{
+        }
+        else
+        {
             Debug.LogError("Attempted to assign second Cowboy.S");
         }
 
-        rb = GetComponent<Rigidbody>();
-        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
 
+        visual = transform.Find("Sprite"); //change "Sprite" to your child object name
+        if (visual != null)
+        {
+            originalScale = visual.localScale;
+            spriteAnimator = visual.GetComponent<SpriteAnimator>();
+        }
+        else
+        {
+            Debug.LogWarning("Visual child object 'Sprite' not found. Flip will not work.");
+        }
     }
 
     // Update is called once per frame
@@ -41,59 +58,79 @@ public class Cowboy : MonoBehaviour
         float hAxis = Input.GetAxis("Horizontal");
         // float vAxis = Input.GetAxis("Vertical");
 
-        Vector3 velocity = rb.linearVelocity;
-        velocity.x = hAxis*speed;
-        velocity.z = 0f;
+        Vector2 velocity = rb.linearVelocity;
+        velocity.x = hAxis * speed;
+        // velocity.z = 0f;
         rb.linearVelocity = velocity;
 
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded) {
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            isGrounded = false;
+        // Flip character sprite left/right
+        if (visual != null && Mathf.Abs(hAxis) > 0.01f)
+        {
+            spriteAnimator.FlipSprite(hAxis);
         }
 
-        if (Input.GetKeyDown(KeyCode.F)) {
-            Debug.Log("F key pressed - triggering Attack1");
-            anim.SetTrigger("Attack1");
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            isGrounded = false;
         }
 
         // Vector3 pos = transform.position;
         // pos.x += hAxis * speed * Time.deltaTime;
         // pos.y += vAxis * speed * Time.deltaTime;
         // transform.position = pos;
-        
+
+        // Set animation state
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            spriteAnimator.PlayAnimation("attack");
+        }
+        else if (!isGrounded)
+        {
+            spriteAnimator.PlayAnimation("jump");
+        }
+        else if (Mathf.Abs(hAxis) > 0.1f)
+        {
+            spriteAnimator.PlayAnimation("run");
+        }
+        else
+        {
+            spriteAnimator.PlayAnimation("idle");
+        }
     }
 
-    void OnTriggerEnter(Collider other)
+    void OnTriggerEnter2D(Collider2D other)
     {
         Transform rootT = other.gameObject.transform.root;
         GameObject go = rootT.gameObject;
-        
 
         // make sure it's not the same triggering go as last time
-        if ( go == lastTriggerGo ) return;
+        if (go == lastTriggerGo) return;
         lastTriggerGo = go;
 
         Enemy enemy = go.GetComponent<Enemy>();
         //PowerUp pUp = go.GetComponent<PowerUp>();
-        if (enemy != null) {
+        if (enemy != null)
+        {
             _shieldLevel--;
-            Destroy(go); 
+            Destroy(go);
             Destroy(this.gameObject);
             Main.HERO_DIED();
         }
         // else if (pUp != null) {    // if shield hit a powerup absorb the powerup
         //     AbsorbPowerUp(pUp);
         // }
-        else {
-            Debug.LogWarning("Shield trigger hit by non-Enemy: " +go.name);
+        else
+        {
+            Debug.LogWarning("Shield trigger hit by non-Enemy: " + go.name);
         }
     }
 
-    void OnCollisionEnter(Collision coll) {
-    if (coll.gameObject.CompareTag("Ground")) {
-        isGrounded = true;
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
         }
     }
-
 }
-
